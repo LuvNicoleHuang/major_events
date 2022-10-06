0b53f3cd447202e8abf3b01d9d2889da2169dffc
huangsiyi <17856749971@163.com>
2022-10-06 16:18:00 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,17 @@
 // 每次调用get、post或ajax的时候会先调用ajaxPrefilter这个函数
 $.ajaxPrefilter(function(options) {
   options.url = 'http://big-event-vue-api-t.itheima.net'+options.url
+
+  if(options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization:localStorage.getItem('token') || ''
+    }
+  }
+
+  options.complete = function(res) {
+    if(res.responseJSON.code === 1 && res.responseJSON.message === '身份认证失败！') {
+      localStorage.removeItem('token')
+      location.href = '/login.html'
+    }
+  }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,44 @@
 $(function() {
   getUserInfo()
 
-
+  const layer = layui.layer
+  $('#btnLogout').on('click',function() {
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, 
+    function(index) {
+      // 清空本地存储中的token
+      localStorage.removeItem('token')
+      // 重新跳转到登录页面
+      location.href = '/login.html'
+      // 关闭confirm询问框
+      layer.close(index)
+    })
+  })
 
 
 
 })
 
+// 获取用户基本信息
 function getUserInfo() {
   $.ajax({
     type:'GET',
     url:'/my/userinfo',
     // headers:请求头配置对象
-    headers:{
-      Authorization:localStorage.getItem('token') || ''
-    },
+    // headers:{
+    //   Authorization:localStorage.getItem('token') || ''
+    // },
     success:function(res) {
       if(res.code !== 0) return layui.layer.msg('获取用户信息失败！')
       renderAvatar(res.data)
-    }
+    },
+    // 在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+    // complete:function(res) {
+    //   console.log(res)
+    //   if(res.responseJSON.code === 1 && res.responseJSON.message === '身份认证失败！') {
+    //     localStorage.removeItem('token')
+    //     location.href = '/login.html'
+    //   }
+    // }
   })
 }
 
